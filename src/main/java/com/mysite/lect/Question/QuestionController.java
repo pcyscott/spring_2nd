4bde61026d8a8857d7ccb02ccd6623779e77ebf2
pcyscott <pcyscott@gmail.com>
2025-01-07 17:33:04 +0900

    list 1

--- src/main/java/com/mysite/lect/Question/QuestionController.java
+++ src/main/java/com/mysite/lect/Question/QuestionController.java
@@ -1,4 +1,12 @@
 package com.mysite.lect.Question;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class QuestionController {
+    @GetMapping("/question/list")
+    public String list() {
+        return "question list";
+    }
 }
