7211690f3132e0c0fcd99dcb515f362030f736ca
pcyscott <pcyscott@gmail.com>
2025-01-08 12:46:07 +0900

    adding answer2

--- src/main/java/com/mysite/lect/Answer/AnswerService.java
+++ src/main/java/com/mysite/lect/Answer/AnswerService.java
@@ -1,4 +1,21 @@
 package com.mysite.lect.Answer;
 
+import com.mysite.lect.Question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateTime(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
 }
