c6726dd8abbe51d86f82071f0d20770c8bdbe95f
pcyscott <pcyscott@gmail.com>
2025-01-08 09:42:52 +0900

    service1

--- src/main/java/com/mysite/lect/Question/QuestionService.java
+++ src/main/java/com/mysite/lect/Question/QuestionService.java
@@ -1,4 +1,16 @@
 package com.mysite.lect.Question;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();   
+    }
 }
