9564b6f9ca9b553dc32200104798b92180212b0c
pcyscott <pcyscott@gmail.com>
2025-01-10 23:36:14 +0900

    modifying answer

--- src/main/java/com/mysite/lect/Answer/AnswerService.java
+++ src/main/java/com/mysite/lect/Answer/AnswerService.java
@@ -2,10 +2,12 @@ package com.mysite.lect.Answer;
 
 import com.mysite.lect.Question.Question;
 import com.mysite.lect.User.SiteUser;
+import com.mysite.lect.Util.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +20,21 @@ public class AnswerService {
         answer.setCreateTime(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Long id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        }else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyTime(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
